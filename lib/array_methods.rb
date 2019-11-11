a125a191ade7db1a6feaf86a13cde7df49383e5a
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-11 05:15:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,9 @@
 def using_include(array, element)
-  #array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.include?(element)
 end
 
 def using_sort(array)
-  #array = 
+  array.sort
 end
 
 def using_reverse(array)
